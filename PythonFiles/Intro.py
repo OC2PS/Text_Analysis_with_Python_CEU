991c20452263ef6e01af482c77736bad382ea02b
Arieda Muco <32225774+ariedamuco@users.noreply.github.com>
2019-04-28 12:03:10 +0200

    Update Intro.py

--- PythonFiles/Intro.py
+++ PythonFiles/Intro.py
@@ -4,22 +4,18 @@ Variables in Python can be considered as containers. They contain everything.
 By assigning different data types to variables, you can store integers, decimals or characters in these variables.
 """
 
-FlowerPrice=3.5
-#In Python you can overwrite a variable
-FlowerPrice=5
+FlowerPrice=3.5 # a float
+FlowerType="Tuilpan" #a string
 
-FlowerType="Tuilpan"
+#In Python you can overwrite a variable
+FlowerPrice=5 # an integer
 
 print (type(FlowerPrice))
 print (type(FlowerType))
 
-#Also convert it to string 
+#Also convert an integer to string 
 
 FlowerPrice=str(FlowerPrice)
 
 #And convert it back to integer (or float)
-FlowerPrice=int(str(FlowerPrice))
-
-#FlowerPrice=float(str(FlowerPrice))
-
-print("Hello World!!!")
+FlowerPrice=int(str(FlowerPrice)) or #FlowerPrice=float(str(FlowerPrice))
