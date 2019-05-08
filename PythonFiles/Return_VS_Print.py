1e7a2ea027a706b643d88c4bf333eff1ec1ebacd
Arieda Muco <32225774+ariedamuco@users.noreply.github.com>
2019-05-08 21:43:55 +0200

    Return vs Print

---
+++ PythonFiles/Return_VS_Print.py
@@ -0,0 +1,16 @@
+def return_cost(price, quantity):
+    cost=price*quantity
+    return cost
+
+
+def print_cost(price, quantity):
+    cost=price*quantity
+    print(cost)
+
+    
+return_cost(10,2)
+print_cost(10,2)
+
+2*return_cost(10,2)
+
+2*print_cost(10,2)
