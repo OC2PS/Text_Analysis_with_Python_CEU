4841117c99172e801e581196b0b1f63179338a76
Arieda Muco <32225774+ariedamuco@users.noreply.github.com>
2019-05-21 13:51:00 +0200

    Update NaiveBayes.py

--- PythonFiles/NaiveBayes.py
+++ PythonFiles/NaiveBayes.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Naive Bayes and Text Analysis
-@author: ariedamuco
-"""
-
 # ONLY RUN THIS CELL IF YOU NEED 
 # TO DOWNLOAD NLTK AND HAVE CONDA
 
